34b59e22653a4017802cdbb5f2cb144a05531436
sandra <Sandra@DESKTOP-L82C1VF>
2021-10-05 14:55:57 +0200

    intento cambio desde windows

--- Boletin1/ejercicio08.py
+++ Boletin1/ejercicio08.py
@@ -7,3 +7,4 @@
  Diseñar un algoritmo que nos diga el dinero que tenemos (en euros y céntimos) después de
 pedirnos cuantas monedas tenemos de 2e, 1e, 50 céntimos, 20 céntimos o 10 céntimos).
 '''
+222
