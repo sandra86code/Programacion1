91222effbf30903ea3282d47b40bcb6a5f536634
sandra <sandra.ruizjimenez86@gmail.com>
2021-10-18 12:26:40 +0200

    Ampliado con bucle for

--- Boletin1/ejercicio09.py
+++ Boletin1/ejercicio09.py
@@ -11,11 +11,11 @@ exponente. Pueden ocurrir tres cosas:
 ◦ El exponente sea negativo, el resultado es 1/potencia con el exponente positivo.
 '''
 
-
+'''
 #Petición de datos
 base=int(input("Introduce la base: "))
 exponente=int(input("Introduce el exponente: "))
-
+ 
 #Expresión lógica para calcular la potencia dependiendo del exponente
 if exponente==0:
     potencia = 1
@@ -23,6 +23,23 @@ elif exponente<0:
     potencia = 1 / (base**abs(exponente))
 else:
     potencia = base**exponente
-
+ 
 #Mostrar resultado por consola
+print("El resultado de la potencia es %s" %(round(potencia, 2)))
+'''
+
+
+#Solución con bucles
+base=int(input("Introduce la base: "))
+exponente=int(input("Introduce el exponente: "))
+
+potencia=1
+if exponente<0:
+    for i in range (abs(exponente)):
+        potencia*=base
+    potencia=1/potencia
+elif exponente>0:
+    for i in range (exponente):
+        potencia*=base
+    
 print("El resultado de la potencia es %s" %(round(potencia, 2)))
