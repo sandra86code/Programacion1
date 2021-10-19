8cee2a5013c3cdc60b5d077122652d12e846d4ca
sandra <sandra.ruizjimenez86@gmail.com>
2021-10-19 09:13:54 +0200

    Corregido

--- Boletin1/ejercicio20.py
+++ Boletin1/ejercicio20.py
@@ -11,17 +11,19 @@ de lo que pagará después de los 20 meses.
 '''
 
 #Creación de variables contadoras usadas en el bucle
-precioMes=0
+precioMes=10
 precioTotal=0
 
 #Creamos el bucle con 20 iteraciones (20 meses)
 for i in range(20):
-    #Incremento en 10 del pago mensual por cada iteración
-    precioMes+=10
     #Mostramos por pantalla el resultado del pago mensual
     print("El mes %s pagó %s €." %(i+1, precioMes))
     #Sumamos el precio mensual por cada iteración
     precioTotal+=precioMes
+    #Incremento en 10 del pago mensual por cada iteración
+    precioMes*=2
+    
 
 #Mostramos por pantalla el precio total a pagar
 print("El precio total a pagar en 20 meses es: %s €." %(precioTotal))
+
