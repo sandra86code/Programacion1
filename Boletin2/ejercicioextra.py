6c201cf07107369afa38057282abe7d05d4460ad
sandra <sandra.ruizjimenez86@gmail.com>
2021-10-22 10:08:48 +0200

    Actualizado

---
+++ Boletin2/ejercicioextra.py
@@ -0,0 +1,30 @@
+"""
+# coding: utf-8
+ @autor: Sandra Ruiz Jimenez
+ @fecha: Oct 22, 2021
+ @nombre: Boletin 2 - Ejercicio Extra
+ @enunciado: 
+ Crea una programa que tenga muestre un mensaje para elegir las siguientes opciones:
+ 
+1. esCapicua: Devuelve verdadero si el número que se pasa como parámetro es capicúa y falso en caso contrario. 
+
+2. siguientePrimo: Devuelve el menor primo que es mayor al número que se pasa como parámetro. 
+
+3. digitos: Cuenta el número de dígitos de un número entero. 6. voltea: Le da la vuelta a un número. 
+
+4. digitoN: Devuelve el dígito que está en la posición n de un número entero. Se empieza contando por el 0 y de izquierda a derecha. 
+
+5. posicionDeDigito: Da la posición de la primera ocurrencia de un dígito dentro de un número entero. Si no se encuentra, devuelve -1. 
+
+6. quitaPorDetras: Le quita a un número n dígitos por detrás (por la derecha). 
+
+7. quitaPorDelante: Le quita a un número n dígitos por delante (por la izquierda). 
+
+8. pegaPorDetras: Añade un dígito a un número por detrás. 
+
+9. pegaPorDelante: Añade un dígito a un número por delante. 
+
+10. trozoDeNumero: Toma como parámetros las posiciones inicial y final dentro de un número y devuelve el trozo correspondiente. 
+
+11. juntaNumeros: Pega dos números para formar uno.
+ """
