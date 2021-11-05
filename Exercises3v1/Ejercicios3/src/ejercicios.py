fa6af9abbe9d3e44fe92d3bdf910525ba8f1ae21
sandra <sandra.ruizjimenez86@gmail.com>
2021-11-05 13:16:30 +0100

    clase 5/11

--- Exercises3v1/Ejercicios3/src/ejercicios.py
+++ Exercises3v1/Ejercicios3/src/ejercicios.py
@@ -216,7 +216,7 @@ def isPrime(num):
  @nombre: Boletin 3 - Ejercicio 8
  @enunciado: 
  Design a method called secondOrder that receives three integer positive number 
- as parameters. 
+ as parameters.
  This parameters are the coefficients of the an equation of a 
  second order (ax2+bx+c=0) and the method returns the numbers of the solutions. 
  If the parameters are not valid the method should return -1.
