ca780ca573279c9fef4db2d050a55cc4b3be3c54
sandra <sandra.ruizjimenez86@gmail.com>
2021-11-09 08:36:03 +0100

    Clase 9/11

--- Boletin4/ejercicio03.py
+++ Boletin4/ejercicio03.py
@@ -9,7 +9,38 @@
  y muestre su área y perímetro.
 '''
 
+#Importo pi de la librería math
+from math import pi
+
+'''
+Esta función calcula el área de una circunferencia
+Recibe el radio de la circunferencia
+Devuelve:
+El área de la circunferencia redondeada a 2 decimales
+'''
+def calculaAreaCircunferencia(radio):
+
+    return round((radio**2)*pi, 2)
 
 
+'''
+Esta función calcula el perímetro de una circunferencia
+Recibe el radio de la circunferencia
+Devuelve:
+El perímetro de la circunferencia redondeada a 2 decimales
+'''
+def calculaPerimetroCircunferencia(radio):
+    
+    return round(2*pi*radio, 2)
 
 
+#Petición y comprobación de datos
+radio=int(input("Introduce el lado de la circunferencia: "))
+while radio<=0:
+    print("Datos incorrectos. Introduce un valor positivo.")
+    radio=int(input("Introduce el lado de la circunferencia: "))
+    
+    
+#Imprimo por consola el aŕea y el perímetro llamando a las funciones con el valor de la variable introducida en línea 38
+print("Él área de la circunferencia es %s " %(calculaAreaCircunferencia(radio)))
+print("Él perímetro de la circunferencia es %s " %(calculaPerimetroCircunferencia(radio)))
