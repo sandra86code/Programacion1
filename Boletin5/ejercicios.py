b246be281ba8a4e58607e0fcbedd6c9f45297c45
sandra <Sandra@SANDRAHOME>
2021-11-11 16:21:26 +0100

    Enunciados creados

---
+++ Boletin5/ejercicios.py
@@ -0,0 +1,124 @@
+'''
+# coding: utf-8 
+ @autor: Sandra Ruiz Jimenez
+ @fecha: Oct 5, 2021
+ @nombre: Boletin 5 (String) - Ejercicio 1
+ @enunciado: 
+ Design a function called charactersInString that has a character string and a character as
+input parameters and returns how many times that character appears in the string. It should
+do if the string and character are lower case or upper case characters.
+'''
+
+
+'''
+# coding: utf-8 
+ @autor: Sandra Ruiz Jimenez
+ @fecha: Oct 5, 2021
+ @nombre: Boletin 5 (String) - Ejercicio 2
+ @enunciado: 
+ Design a function called lowCaseInString that has a string of characters as parameter, the
+method should return how many of those characters are lowcase letters.
+'''
+
+
+'''
+# coding: utf-8 
+ @autor: Sandra Ruiz Jimenez
+ @fecha: Oct 5, 2021
+ @nombre: Boletin 5 (String) - Ejercicio 3
+ @enunciado: 
+ Design a function called upperCaseInString that has a string of characters as parameter, the
+method should return how many of those characters are upper case letters.
+'''
+
+
+
+'''
+# coding: utf-8 
+ @autor: Sandra Ruiz Jimenez
+ @fecha: Oct 5, 2021
+ @nombre: Boletin 5 (String) - Ejercicio 4
+ @enunciado: 
+ Design a function called numberInString that has a string of characters as parameter, the
+method should return how many of those characters are numbers.
+'''
+
+
+'''
+# coding: utf-8 
+ @autor: Sandra Ruiz Jimenez
+ @fecha: Oct 5, 2021
+ @nombre: Boletin 5 (String) - Ejercicio 5
+ @enunciado: 
+ Design a function called palindrome that has a string of characters as parameter, and return
+true if it is a palindrome or false in other case. A word is a palindrome, if it is reads the
+same from left to right as from right to left, ignoring whites,. For example: "anilina" or "el
+abad le dio arroz al zorro" To simplify the problem, you can assume that simple characters
+are used, that is, without tildes or diresis.
+'''
+
+
+'''
+# coding: utf-8 
+ @autor: Sandra Ruiz Jimenez
+ @fecha: Oct 5, 2021
+ @nombre: Boletin 5 (String) - Ejercicio 6
+ @enunciado: 
+ Realizar una función que busque una palabra escondida dentro de un texto. 
+ Por ejemplo, si la cadena es "shybaoxlna" y la palabra que queremos buscar 
+ es "hola", entonces si se encontrará y deberá devolver True, en caso 
+ contrario deberá devolver False.
+'''
+
+
+
+'''
+# coding: utf-8 
+ @autor: Sandra Ruiz Jimenez
+ @fecha: Oct 5, 2021
+ @nombre: Boletin 5 (String) - Ejercicio 7
+ @enunciado: 
+ Diseñar una función que reciba como parámetro tres cadenas, la primera será una frase y
+deberá buscar si existe la palabra que recibe como segundo parámetro y reemplazarla por la
+tercera.
+'''
+
+
+
+'''
+# coding: utf-8 
+ @autor: Sandra Ruiz Jimenez
+ @fecha: Oct 5, 2021
+ @nombre: Boletin 5 (String) - Ejercicio 8
+ @enunciado: 
+ Diseñar una función que determine la cantidad de vocales diferentes, que tiene una palabra
+o frase introducida por teclado. Por ejemplo, la cadena "Abaco", devolverá 2.
+'''
+
+
+'''
+# coding: utf-8 
+ @autor: Sandra Ruiz Jimenez
+ @fecha: Oct 5, 2021
+ @nombre: Boletin 5 (String) - Ejercicio 9
+ @enunciado: 
+ Crear una función que, tomando una cadena de texto como entrada, construya y devuelva
+otra cadena formada de la siguiente manera: el método debe colocar todas las consonantes
+al principio y todas las vocales al final de la misma, eliminando los blancos.
+Por ejemplo, pasándole la cadena "curso de programacion", una posible solución será
+"crsdprgrmcnuoeoaaio".
+'''
+
+
+
+'''
+# coding: utf-8 
+ @autor: Sandra Ruiz Jimenez
+ @fecha: Oct 5, 2021
+ @nombre: Boletin 5 (String) - Ejercicio 10
+ @enunciado: 
+ Escribir una función que, devuelva el número de palabras que hay en una cadena que recibe
+como parámetro. Ten en cuenta que entre dos palabras puede haber más de un blanco.
+También al principio y al final de la frase puede haber blancos redundantes.
+Por ejemplo, si la cadena es "He estudiado mucho", debe devolver 3.
+'''
