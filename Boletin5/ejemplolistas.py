ed9925aa9802c238d2fed787b736fe30dcbfa627
sandra <Sandra@SANDRAHOME>
2021-11-12 19:48:03 +0100

    Actualizado

--- Boletin5/ejemplolistas.py
+++ Boletin5/ejemplolistas.py
@@ -1,33 +1,25 @@
+#Código que elimina espacios y el retorno de carro
+nombre = input("Introduce tu nombre: ")
+for i in range (len(nombre)):
+    if nombre[i] != ' ':
+        print(nombre[i], end='')
 
 
-for i in range (0,4, -1):
-    print(i)
+'''
+Ejercicio que solicite nombre y lo guarde en una lista para imprimirlos
+luego.
+Después de cada nombre debe preguntar si quiere introducir más o no (s/n)
+'''
+names=[]
 
+respuesta='s'
+while respuesta=='s':
+    nombre=input("Introduce un nombre: ")
+    names.append(nombre)
+    respuesta=input("Quieres continuar? (s/n): ")
+    while respuesta not in ['s', 'n']:
+        respuesta=input("Quieres continuar? (s/n): ")
 
-
-
-# #Código que elimina espacios y el retorno de carro
-# nombre = input("Introduce tu nombre: ")
-# for i in range (len(nombre)):
-#     if nombre[i] != ' ':
-#         print(nombre[i], end='')
-#
-#
-# '''
-# Ejercicio que solicite nombre y lo guarde en una lista para imprimirlos
-# luego.
-# Después de cada nombre debe preguntar si quiere introducir más o no (s/n)
-# '''
-# names=[]
-#
-# respuesta='s'
-# while respuesta=='s':
-#     nombre=input("Introduce un nombre: ")
-#     names.append(nombre)
-#     respuesta=input("Quieres continuar? (s/n): ")
-#     while respuesta not in ['s', 'n']:
-#         respuesta=input("Quieres continuar? (s/n): ")
-#
-# print(names)
+print(names)
 
 
