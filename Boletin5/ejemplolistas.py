8a109f7ae850d1eadf9f4dd1c7d58867837065a9
sandra <sandra.ruizjimenez86@gmail.com>
2021-11-12 09:54:56 +0100

    Actualizado

--- Boletin5/ejemplolistas.py
+++ Boletin5/ejemplolistas.py
@@ -1,25 +1,33 @@
-#Código que elimina espacios y el retorno de carro
-nombre = input("Introduce tu nombre: ")
-for i in range (len(nombre)):
-    if nombre[i] != ' ':
-        print(nombre[i], end='')
 
 
-'''
-Ejercicio que solicite nombre y lo guarde en una lista para imprimirlos
-luego.
-Después de cada nombre debe preguntar si quiere introducir más o no (s/n)
-'''
-names=[]
+for i in range (0,4, -1):
+    print(i)
 
-respuesta='s'
-while respuesta=='s':
-    nombre=input("Introduce un nombre: ")
-    names.append(nombre)
-    respuesta=input("Quieres continuar? (s/n): ")
-    while respuesta not in ['s', 'n']:
-        respuesta=input("Quieres continuar? (s/n): ")
-    
-print(names)
+
+
+
+# #Código que elimina espacios y el retorno de carro
+# nombre = input("Introduce tu nombre: ")
+# for i in range (len(nombre)):
+#     if nombre[i] != ' ':
+#         print(nombre[i], end='')
+#
+#
+# '''
+# Ejercicio que solicite nombre y lo guarde en una lista para imprimirlos
+# luego.
+# Después de cada nombre debe preguntar si quiere introducir más o no (s/n)
+# '''
+# names=[]
+#
+# respuesta='s'
+# while respuesta=='s':
+#     nombre=input("Introduce un nombre: ")
+#     names.append(nombre)
+#     respuesta=input("Quieres continuar? (s/n): ")
+#     while respuesta not in ['s', 'n']:
+#         respuesta=input("Quieres continuar? (s/n): ")
+#
+# print(names)
 
 
--- Boletin5/ejercicios.py
+++ Boletin5/ejercicios.py
@@ -5,9 +5,118 @@
  @nombre: Boletin 5 (String) - Ejercicio 1
  @enunciado: 
  Design a function called charactersInString that has a character string and a character as
-input parameters and returns how many times that character appears in the string. It should
-do if the string and character are lower case or upper case characters.
+input parameters and returns how many times that character appears in the string. 
+It should do if the string and character are lower case or upper case characters.
 '''
+'''
+Esta función calcula 
+Recibe 
+Devuelve:
+
+'''
+'''
+Esta función calcula si un caracter es minúsculas
+Recibe un caracter
+Devuelve:
+True si el caracter está en minúsculas (usando la función ord() y el número en el código ASCII)
+False si el caracter no está en minúsculas
+
+'''
+def isMinusculas(caracter):
+    isMinusculas=False
+    if ord(caracter)>=97 and ord(caracter)<=122:
+        isMinusculas=True
+        
+    return isMinusculas
+
+
+'''
+Esta función calcula el número de coincidencias de un caracter en una cadena
+Recibe una cadena y un caracter
+Devuelve:
+El número de coincidencias de un caracter en una cadena
+'''
+def charactersInString(cadena, caracter):
+    resultado=0
+    #Estructura condicional en que si el resultado de la llamada a la otra función es True 
+    #(caracter es minúsculas, se le quitan -32 para que coincide con el mismo caracter en
+    #mayúsculas
+    if isMinusculas(caracter):
+        suma = -32          
+    #De lo contrario (el caracter no es minúsculas, se le suman 32.
+    else:
+        suma = 32
+    
+    #Recorro cadena y compruebo que el codigo ASCII de cada letra de la cadena es igual a
+    #código ASCII del caracter o que es igual a la suma (-32 o 32).
+    for i in cadena:
+        if ord(i)==ord(caracter) or ord(i)==ord(caracter)+suma:
+            resultado+=1
+            
+    return resultado
+
+assert(charactersInString('Stranger Things', 'u')==0)
+assert(charactersInString('Stranger Things', 'a')==1)
+assert(charactersInString('Stranger Things', 's')==2)
+assert(charactersInString('Stranger Things', 'H')==1)
+
+
+'''
+# coding: utf-8 
+ @autor: Sandra Ruiz Jimenez
+ @fecha: Oct 5, 2021
+ @nombre: Boletin 5 (String) - Ejercicio 2
+ @enunciado: 
+ Diseña una función que reciba una cadena y la devuelva en minúsculas y otra función 
+ que la devuelva en mayúsculas:
+'''
+
+'''
+Esta función devuelve una cadena convertida a minúsculas (funcion cadena.lower())
+Recibe una cadena
+Devuelve: la cadena en minúsculas
+
+'''
+def convierteAMinusculas(cadena):
+    cadenaConvertida=''
+    #Recorro la cadena
+    for i in cadena:
+        #si i es minúsculas o es un espacio, lo acumulo en la cadena convertida
+        if isMinusculas(i)==True or i==' ':
+            cadenaConvertida+=i
+        #Si i es mayúsculas, lo convierto a minúsculas usando ord() y luego como esto es
+        #un ordinal y yo necesito el string, uso chr() y lo acumulo en la cadena convertida
+        else:
+            cadenaConvertida+=chr(ord(i)+32)
+  
+    return cadenaConvertida
+
+
+assert(convierteAMinusculas('La ONU es internacional')=='la onu es internacional')
+assert(convierteAMinusculas('CadeNA')=='cadena')
+
+
+'''
+Esta función devuelve una cadena convertida a mayúsculas (funcion cadena.upper())
+Recibe una cadena
+Devuelve: la cadena en mayúsculas
+
+'''
+def convierteAMayusculas(cadena):
+    cadenaConvertida=''
+    for i in cadena:
+        #si i es un espacio o es mayúscula, lo acumulo en la cadena convertida
+        if i==' ' or isMinusculas(i)==False:
+            cadenaConvertida+=i
+        #Si i es minúsculas, lo convierto a mayúsculas usando ord() y luego como esto es
+        #un ordinal y yo necesito el string, uso chr() y lo acumulo en la cadena convertida
+        else:
+            cadenaConvertida+=chr(ord(i)-32)
+  
+    return cadenaConvertida
+
+assert(convierteAMayusculas('La ONU es internacional')=='LA ONU ES INTERNACIONAL')
+assert(convierteAMayusculas('CadeNA')=='CADENA')
 
 
 '''
@@ -18,9 +127,27 @@ do if the string and character are lower case or upper case characters.
  @enunciado: 
  Design a function called lowCaseInString that has a string of characters as parameter, the
 method should return how many of those characters are lowcase letters.
+#CÓDIGO ASCII: a-z es [97-122], 
+ñ = 241
+estos números se sacan con ord('A')
 '''
 
+'''
+Esta función calcula cuántas letras en minúscula hay en una cadena (no controla tildes ni ñ)
+Recibe una cadena (variable string)
+Devuelve: el número de letras en minúscula de la cadena (sin contar letras con tildes o ñ)
+'''
+def lowCaseInString(cadena):
+    resultado=0
+    for i in cadena:
+        if (i>=chr(97) and i<=chr(122)) or i==chr(241):
+            resultado+=1
+    return resultado
 
+assert(lowCaseInString("clase de programacion")==19)
+assert(lowCaseInString("Hola")==3)
+assert(lowCaseInString("ONU")==0)
+assert(lowCaseInString("Guiso de ñame")==10)
 '''
 # coding: utf-8 
  @autor: Sandra Ruiz Jimenez
@@ -29,9 +156,28 @@ method should return how many of those characters are lowcase letters.
  @enunciado: 
  Design a function called upperCaseInString that has a string of characters as parameter, the
 method should return how many of those characters are upper case letters.
+#CÓDIGO ASCII: A-Z es [65 a 90]
+Ñ= 209
+estos números se sacan con ord('A')
 '''
 
+'''
+Esta función calcula cuántas letras en mayúscula hay en una cadena (no controla tildes ni ñ)
+Recibe una cadena (variable string)
+Devuelve: el número de letras en mayúscula de la cadena (sin contar letras con tildes o ñ)
+'''
+def upperCaseInString(cadena):
+    resultado=0
+    for i in cadena:
+        if (i>=chr(65) and i<=chr(90)) or i==chr(209):
+            resultado+=1
+    return resultado
 
+
+assert(upperCaseInString("clase de programacion")==0)
+assert(upperCaseInString("Hola")==1)
+assert(upperCaseInString("ONU")==3)
+assert(upperCaseInString("Sandra Ñantes")==2)
 
 '''
 # coding: utf-8 
@@ -41,8 +187,26 @@ method should return how many of those characters are upper case letters.
  @enunciado: 
  Design a function called numberInString that has a string of characters as parameter, the
 method should return how many of those characters are numbers.
+#CÓDIGO ASCII: 0-9 es [48-57]
 '''
 
+'''
+Esta función calcula cuántos caracteres numéricos hay en una cadena
+Recibe una cadena
+Devuelve:
+El número de caracteres numéricos que hay en una cadena
+'''
+def numberInString(cadena):
+    resultado=0 
+    for i in cadena:
+        if i>=chr(48) and i<=chr(57):
+            resultado+=1
+            
+    return resultado
+
+assert(numberInString('Los 40 principales')==2)
+assert(numberInString('kdi2335kik213')==7)
+assert(numberInString('clase de programacion')==0)
 
 '''
 # coding: utf-8 
@@ -51,12 +215,23 @@ method should return how many of those characters are numbers.
  @nombre: Boletin 5 (String) - Ejercicio 5
  @enunciado: 
  Design a function called palindrome that has a string of characters as parameter, and return
-true if it is a palindrome or false in other case. A word is a palindrome, if it is reads the
-same from left to right as from right to left, ignoring whites,. For example: "anilina" or "el
-abad le dio arroz al zorro" To simplify the problem, you can assume that simple characters
-are used, that is, without tildes or diresis.
+true if it is a palindrome or false in other case. 
+A word is a palindrome, if it is reads the same from left to right as from right to left, 
+ignoring whites,. For example: "anilina" or "el abad le dio arroz al zorro".
+To simplify the problem, you can assume that simple characters are used, that is, without 
+tildes or diresis.
 '''
 
+def palindrome(cadena):
+    cadenaReves=''
+    for i in cadena:
+        cadenaReves+=
+    print(cadenaReves)
+    return cadenaReves
+
+assert(palindrome('Ave')==False)
+assert(palindrome('lavan esa base naval')==True)
+assert(palindrome('el abad le dio arroz al zorro')==True)
 
 '''
 # coding: utf-8 
@@ -69,7 +244,9 @@ are used, that is, without tildes or diresis.
  es "hola", entonces si se encontrará y deberá devolver True, en caso 
  contrario deberá devolver False.
 '''
-
+# def wordInText(palabra, texto):
+#
+#     return resultado
 
 
 '''
