3f17ae5ee7cece6a9e9143588e9f540c7dbe79a0
sandra <sandra.ruizjimenez86@gmail.com>
2021-11-19 11:33:22 +0100

    Por terminar, faltan asserts y comentarios

--- Boletin7/ejercicio11.py
+++ Boletin7/ejercicio11.py
@@ -7,3 +7,27 @@
 Escribe una función que reciba dos listas y devuelva una lista con los 
 elementos comunes a ambas, sin repetir ninguno.
 '''
+
+def listarElementosComunes(lista1, lista2):
+    listaComunes=[]
+    if lista1>lista2:
+        listaMayor=lista1
+        listaMenor=lista2
+    else:
+        listaMayor=lista2
+        listaMenor=lista1
+        
+    for i in listaMenor:
+        j=0
+        itemFound=False
+        while j<len(listaMayor) and itemFound==False:
+            if listaMenor[i]==listaMayor[j]:
+                listaComunes.append(listaMenor[i])
+                itemFound==True
+            
+            else:
+                j+=1
+    
+    return listaComunes
+
+assert(listarElementosComunes([], [])==[])
