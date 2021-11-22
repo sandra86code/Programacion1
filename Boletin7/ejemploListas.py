53fdc584068dee62e817e6e3adc29e76ec5f213d
sandra <Sandra@SANDRAHOME>
2021-11-22 17:56:55 +0100

    Actualizado

--- Boletin7/ejemploListas.py
+++ Boletin7/ejemploListas.py
@@ -1,10 +1,16 @@
 x=[1,2,3,4]
+print(id(x))
 
 def prueba(x):
-    x[1]=5
+    x[0]=9
     x=[5,6,7]
-    x[0]=5
+    x[1]=8
+    print(x)
+    print(id(x))
+        
     return x
-    
-x =prueba(x)
+
+x=prueba(x)
+
 print(x)
+print(id(x))
