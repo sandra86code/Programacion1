70842c7aaace1a04b6f2f1967ab5057d2eb833b3
sandra <Sandra@SANDRAHOME>
2021-11-25 19:56:36 +0100

    Nuevo ejercicio, enunciado

---
+++ Boletin7/ejercicio10.py
@@ -0,0 +1,55 @@
+'''
+# coding: utf-8 
+ @autor: Sandra Ruiz Jimenez
+ @fecha: 25 nov 2021
+ @nombre: Cuadrado de números
+ @enunciado: 
+ Cuadrado de números:
+Crea un programa que lea del teclado un número y genere un cuadrado con el patrón siguiente
+donde cada elemento está separado por un espacio.
+
+Resultados de ejemplo:
+
+Número 2:
+2 2 2
+2 1 2
+2 2 2
+____________
+
+Número 4:
+4 4 4 4 4 4 4
+4 3 3 3 3 3 4
+4 3 2 2 2 3 4
+4 3 2 1 2 3 4
+4 3 2 2 2 3 4
+4 3 3 3 3 3 4
+4 4 4 4 4 4 4
+____________
+
+Número 5:
+5 5 5 5 5 5 5 5 5
+5 4 4 4 4 4 4 4 5
+5 4 3 3 3 3 3 4 5
+5 4 3 2 2 2 3 4 5
+5 4 3 2 1 2 3 4 5
+5 4 3 2 2 2 3 4 5
+5 4 3 3 3 3 3 4 5
+5 4 4 4 4 4 4 4 5
+5 5 5 5 5 5 5 5 5
+____________
+
+Número 7:
+7 7 7 7 7 7 7 7 7 7 7 7 7
+7 6 6 6 6 6 6 6 6 6 6 6 7
+7 6 5 5 5 5 5 5 5 5 5 6 7
+7 6 5 4 4 4 4 4 4 4 5 6 7
+7 6 5 4 3 3 3 3 3 4 5 6 7
+7 6 5 4 3 2 2 2 3 4 5 6 7
+7 6 5 4 3 2 1 2 3 4 5 6 7
+7 6 5 4 3 2 2 2 3 4 5 6 7
+7 6 5 4 3 3 3 3 3 4 5 6 7
+7 6 5 4 4 4 4 4 4 4 5 6 7
+7 6 5 5 5 5 5 5 5 5 5 6 7
+7 6 6 6 6 6 6 6 6 6 6 6 7
+7 7 7 7 7 7 7 7 7 7 7 7 7
+'''
