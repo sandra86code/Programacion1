13adb7550f89587067e564027cd5b4992daa5e1c
sandra <sandra.ruizjimenez86@gmail.com>
2021-11-26 09:17:33 +0100

    Terminado, funcionando y comentado

--- Boletin7/ejercicio06.py
+++ Boletin7/ejercicio06.py
@@ -7,3 +7,21 @@
  Crea un programa que calcule cuántos números negativos hay en un array de
 enteros.
 '''
+
+#===============================================================================
+# Esta función calcula cuántos números negativos hay en un array de enteros
+# Recibe: una lista de enteros
+# Devuelve: la cantidad de enteros negativos en la lista
+#===============================================================================
+def calculaNegativosDeLista(lista):
+    cantidadNegativos=0
+    for i in lista:
+        if i<0:
+            cantidadNegativos+=1
+            
+    return cantidadNegativos
+
+
+assert(calculaNegativosDeLista([1,2,3,4,5])==0)
+assert(calculaNegativosDeLista([-1,2,3,4,-5,0])==2)
+assert(calculaNegativosDeLista([-1,2,-3,-4,-5,0])==4)
--- Boletin7/ejercicio07.py
+++ Boletin7/ejercicio07.py
@@ -6,8 +6,9 @@
  @enunciado: 
  Crea un programa que pregunte cuántos elementos tendremos en un array de
 enteros. Posteriormente se preguntarán tantos números como haya indicado el
-usuario. Posteriormente, mostraremos por pantalla el mayor, el menor y la media. Si
-el número mayor o menor están repetidos, deberá indicarse cuántas veces se repite.
+usuario. Posteriormente, mostraremos por pantalla el mayor, el menor y la media. 
+Si el número mayor o menor están repetidos, deberá indicarse cuántas veces se repite.
+
 Por ejemplo:
     Introduzca las posiciones que tendrá el array: 5
     Introduzca el número 1: 4
@@ -19,3 +20,89 @@ Por ejemplo:
     El menor número introducido es -1
     La media de los números introducidos es: 5.2
 '''
+
+#===============================================================================
+# Esta función calcula la media de una lista de números usando la suma de los mismos
+# entre el número de elementos de la lista
+# Recibe: una lista de números
+# Devuelve: la media de los números con un redondeo a 2 decimales
+#===============================================================================
+def calculaMedia(lista):
+    suma=0
+    for i in lista:
+        suma+=i
+    
+    media=round(suma/len(lista),2)
+    
+    
+    return media
+
+#===============================================================================
+# Esta función devuelve el número menor de una lista de números y cuántas veces se
+# repite dicho número menor en la lista
+# Recibe: una lista de números
+# Devuelve: el menor de los números de la lista y el número de repeticiones
+#===============================================================================
+def calculaMenor(lista):
+    menor=lista[0]
+    repeticion=0
+    for i in range(1, len(lista)):
+        if lista[i]<menor:
+            menor=lista[i]
+    
+    for i in range(len(lista)):
+        if lista[i]==menor:
+            repeticion+=1
+            
+    return [menor, repeticion]
+
+assert(calculaMenor([27,8,-3,80,27,80])==[-3, 1])
+assert(calculaMenor([27,8,-3,-80,27,-80])==[-80, 2])
+
+#===============================================================================
+# Esta función calcula el número mayor de una lista de números y cuántas veces se
+# repite el número mayor en la lista
+# Recibe: una lista de números
+# Devuelve: el mayor de los números de la lista y el número de repeticiones
+# 
+#===============================================================================
+def calculaMayor(lista):
+    mayor=lista[0]
+    repeticion=0
+    for i in range (1,len(lista)):
+        if lista[i]>mayor:
+            mayor=lista[i]
+    
+    for i in range(len(lista)):
+        if lista[i]==mayor:
+            repeticion+=1
+    
+    return [mayor, repeticion]
+    
+assert(calculaMayor([27,8,-3,80,27,80])==[80, 2])
+assert(calculaMayor([27,8,-3,8,27,80])==[80, 1])
+
+
+
+#===============================================================================
+# Esta función es el programa principal
+# Recibe 
+# Devuelve:
+# 1. Pide cantidad de números a introducir
+# 2. Pide esa cantidad de números y los introduce en una lista
+# 3. Imprime los resultados del número mayor, el número menor y la media
+#===============================================================================
+def main():
+    cantidadNumeros=int(input("¿Cuántos números quieres introducir? "))
+    listaNumeros=[]
+    for i in range (cantidadNumeros):
+        num=int(input("Introduce el número %s: " %(i+1)))
+        listaNumeros.append(num)
+
+
+    print("El mayor número introducido es %s (se repite %s veces)" % (calculaMayor(listaNumeros)[0], calculaMayor(listaNumeros)[1]))
+    print("El menor número introducido es %s (se repite %s veces)" % (calculaMenor(listaNumeros)[0], calculaMenor(listaNumeros)[1]))
+    print("La media de los números introducidos es: %s" % (calculaMedia(listaNumeros)))
+    
+    
+main()
