398c32504187222b240c45377b58c55b03628298
sandra <sandra.ruizjimenez86@gmail.com>
2021-11-26 12:24:04 +0100

    Cuadrado vacío creado y accedido. Falta meter números y pasarlo a string

--- Boletin7/ejercicio10.py
+++ Boletin7/ejercicio10.py
@@ -53,3 +53,34 @@ Número 7:
 7 6 6 6 6 6 6 6 6 6 6 6 7
 7 7 7 7 7 7 7 7 7 7 7 7 7
 '''
+
+
+def insertaEnCuadrado(numero, cuadrado):
+    
+    for fila in range (len(cuadrado)):
+        fila=cuadrado[fila]
+        for columna in range (len(fila)):
+            fila[columna]=num
+
+    return cuadrado
+
+
+def creaCuadrado(numero):
+    numFilas=numero+(numero-1)
+    numColumnas=numFilas
+    cuadrado=[]
+    for i in range (numFilas):
+        cuadrado.append([])
+        for j in range (numColumnas):
+            cuadrado[i].append([])
+        
+    return cuadrado
+
+
+num=int(input("Introduce un número entero positivo: "))
+while num<=0:
+    print("El número debe ser un entero positivo (mayor que 0).")
+    num=int(input("Introduce un número entero positivo: "))
+    
+print(insertaEnCuadrado(num, creaCuadrado(num)))
+
