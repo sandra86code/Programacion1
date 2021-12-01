959c77bf27ac3ac42bb524e8452a86cb839d26ea
sandra <Sandra@SANDRAHOME>
2021-12-01 23:11:09 +0100

    Mitad inferior del cuadrado  por hacer

--- Boletin7/ejercicio10.py
+++ Boletin7/ejercicio10.py
@@ -54,54 +54,84 @@ Número 7:
 7 7 7 7 7 7 7 7 7 7 7 7 7
 '''
 
-def convierteCuadrado(lista):
-    cuadrado=""
-    for fila in range (len(lista)):
-        fila=lista[fila]
-        for j in range (len(fila)-1):
-            cuadrado+=str(fila[j])
-            cuadrado+=" "
-        cuadrado+=str(fila[-1])
-        cuadrado+="\n"
-    
-    return cuadrado
-           
+
     
 
-def insertaEnCuadrado(numero, cuadrado):
-    fila=0
-    # while fila<len(cuadrado):
-    #     columna=0
-    #     while columna<len(cuadrado[fila]):
-    #         cuadrado[0+columna][0+columna]=numero-columna
-    #
-    #
-    #
-    #         # if fila-columna==0:
-    #         #     numero-=1
-    #         #     cuadrado[fila][columna]=numero
-    #         columna+=1
-    #     fila+=1
-            
-    return cuadrado
 
 
-def creaCuadrado(numero):
-    numFilas=numero+(numero-1)
-    numColumnas=numFilas
-    cuadrado=[]
-    for i in range (numFilas):
-        cuadrado.append([])
-        for j in range (numColumnas):
-            cuadrado[i].append([])
-        
-    return cuadrado
+
+
+
+# def convierteCuadrado(lista):
+#     cuadrado=""
+#     for fila in range (len(lista)):
+#         fila=lista[fila]
+#         for j in range (len(fila)-1):
+#             cuadrado+=str(fila[j])
+#             cuadrado+=" "
+#         cuadrado+=str(fila[-1])
+#         cuadrado+="\n"
+#
+#     return cuadrado
+#
+#
+#
+# def insertaEnCuadrado(numero, cuadrado):
+#     i=0
+#     while i<len(cuadrado):
+#         fila=numero-i
+#         columna=2*numero-i
+#         cuadrado[fila][columna]
+#     #
+#     #
+#     #
+#     #         # if fila-columna==0:
+#     #         #     numero-=1
+#     #         #     cuadrado[fila][columna]=numero
+#     #         columna+=1
+#     #     fila+=1
+#
+#     return cuadrado
+#
+#
+# def creaCuadrado(numero):
+#     numFilas=numero+(numero-1)
+#     numColumnas=numFilas
+#     cuadrado=[]
+#     for i in range (numFilas):
+#         cuadrado.append([])
+#         for j in range (numColumnas):
+#             cuadrado[i].append([])
+#
+#     return cuadrado
 
 
 num=int(input("Introduce un número entero positivo: "))
 while num<=0:
     print("El número debe ser un entero positivo (mayor que 0).")
     num=int(input("Introduce un número entero positivo: "))
+
+numero=num
+cuadrado=""
+for i in range (num):
+    cantidad=2*(num-i)-1
+    for j in range(i):
+        cuadrado+=str(num-j)
+    for k in range(1, cantidad+1):
+        cuadrado+=str(num-i)
+    for l in range(i):
+        cuadrado+=str(numero-i+l+1)
+    cuadrado+="\n"
+for m in range (2, num+1):
+    cantidad=2*m-1
+    for n in range(m-2):
+        cuadrado+=str(numero-m+n+1)
+    for o in range(1, cantidad+1):
+        cuadrado+=str(m)
+    # for p in range(i):
+    #     cuadrado+=str(num-p)
+    cuadrado+="\n"
     
-print(convierteCuadrado(insertaEnCuadrado(num, creaCuadrado(num))))
+print(cuadrado)
+# print(convierteCuadrado(insertaEnCuadrado(num, creaCuadrado(num))))
 
