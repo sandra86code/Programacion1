aac992847eb9711847e06ed581500766c3c3d475
sandra <sandra.ruizjimenez86@gmail.com>
2021-12-04 13:48:59 +0100

    Terminado, funcionando y comentado

--- Boletin9/ejercicio04.py
+++ Boletin9/ejercicio04.py
@@ -9,7 +9,37 @@
  unos o menos uno, segun el número sea mayor que el siguiente (un 1) o un menor (un -1).
 '''
 
+#===============================================================================
+# Esta función recibe una lista y devuelve otra lista de 1 y -1 según el número
+#sea mayor que el siguiente (1) o menor que el siguiente (-1).
+# Recibe: una lista de números positivos y negativos
+# Devuelve: la lista de -1 y 1.
+#===============================================================================
+def creaLista(lista):
+    listaNueva=[]
+    for i in range (len(lista)-1):
+        if lista[i]>lista[i+1]:
+            listaNueva.append(1)
+        elif lista[i]<lista[i+1]:
+            listaNueva.append(-1)
+        else:
+            listaNueva.append(0)
+            
+    return listaNueva
 
 
-numeros=[]
-
+#===============================================================================
+# Esta función es la principal del programa, donde creamos la lista de números e
+# imprimimos el resultado de la función que crea la lista de -1 y 1
+#===============================================================================
+def main():
+    
+    numeros=[]
+    num=1
+    while num!=0:
+        num=int(input("Introduce un número (0 para parar):"))
+        numeros.append(num)
+    
+    print(creaLista(numeros))
+    
+main()
