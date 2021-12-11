951373078222964abb5dbe2f24242e1b8b453cc1
sandra <sandra.ruizjimenez86@gmail.com>
2021-12-11 11:49:04 +0100

    Terminado, funcionando y comentado

--- RetoCensoElectoral/ejercicio.py
+++ RetoCensoElectoral/ejercicio.py
@@ -95,55 +95,99 @@ A continuación mostramos estos datos de ejemplo para que puedas comprobar tu al
     
 '''
 
-
-def cifra(descifrado):
-    clave=""
+#===============================================================================
+# Esta función corresponde al algoritmo que cifra un número en formato string
+# Recibe: un numero en formato string
+# Devuelve: el numero cifrado en formato string
+#===============================================================================
+def cifra(numeroDescifrado):
+    #Creo el número cifrado vacío
+    numeroCifrado=""
+    #Esta variable es la que va a recoger la cuántas veces se repite un mismo número
+    #para luego ponerla en el número cifrado. La inicializo a 1 porque el dígito siempre
+    #aparece 1 vez.
     cantidad=1
-    for i in range(len(descifrado)-1):
-        if descifrado[i]==descifrado[i+1]:
+    #Recorro la cadena de dígitos hasta su longitud -1, ya que voy a comparar con el número posterior
+    #y sino se saldría de rango
+    for i in range(len(numeroDescifrado)-1):
+        #Si el caracter numerico es el mismo que el que le sigue, aumento la cantidad
+        if numeroDescifrado[i]==numeroDescifrado[i+1]:
             cantidad+=1
-        elif descifrado[i]!=descifrado[i+1]:
-            clave+=str(cantidad)+descifrado[i]
+        #Si es diferente añado al numeroCifrado la cantidad convertida a string y ese dígito
+        else:
+            numeroCifrado+=str(cantidad)+numeroDescifrado[i]
             cantidad=1
-    clave+=str(cantidad)+descifrado[-1]
+    #Al finalizar añado el valor de cantidad y el número de la última posición
+    numeroCifrado+=str(cantidad)+numeroDescifrado[-1]
     
-    return clave
+    return numeroCifrado
 
-assert(cifra("1111")=="41")
-assert(cifra("21")=="1211")
-assert(cifra("13112221")=="1113213211")
+# assert(cifra("1")=="11")
+# assert(cifra("1111")=="41")
+# assert(cifra("21")=="1211")
+# assert(cifra("13112221")=="1113213211")
 
 
-def descifra (clave):
-    descifrado=""
-    if len(clave)>1:
-        for i in range (0,(len(clave)),2):
-            cantidad=int(clave[i])
-            numero=int(clave[i+1])
+#===============================================================================
+# Esta función 
+# Recibe 
+# Devuelve:
+# 
+#===============================================================================
+def descifra (numeroCifrado):
+    numeroDescifrado=""
+    #Si el número es par ejecuto el descifrado
+    if len(numeroCifrado)%2==0:
+        #Recorro el número cifrado (variable string) dígito a dígito en saltos
+        #de 2
+        for i in range (0,(len(numeroCifrado)),2):
+            #La cantidad es el digito y lo convierto a entero porque lo necesito para
+            #otro bucle
+            cantidad=int(numeroCifrado[i])
+            #El número en sí es el siguiente al que corresponde a la cantidad
+            numero=numeroCifrado[i+1]
+            #Creo un nuevo bucle cuyo número de iteraciones es la cantidad, para añadir
+            #el número la cantidad de veces que valga la variable cantidad
             for j in range (cantidad):
-                descifrado+=str(numero)
+                numeroDescifrado+=numero
+    #Si el número es impar, el número descifrado es el mismo que el cifrado
     else:
-        descifrado=clave
-        
-    return descifrado
+        numeroDescifrado=numeroCifrado
 
-assert(descifra("41")=="1111")
-assert(descifra("1211")=="21")
-assert(descifra("11")=="1")
+    return numeroDescifrado
 
+# assert(descifra("41")=="1111")
+# assert(descifra("1211")=="21")
+# assert(descifra("11")=="1")
+# assert(descifra("111")=="111")
+# assert(descifra("1")=="1")
+# assert(descifra("1111111")=="1111111")
+
+
+#===============================================================================
+# Esta función es la principal del programa, la que interactua con el usuario
+# llama a las otras funciones e imprime el valor final
+#===============================================================================
 def main():
-    
+
     clave=input("Introduce la clave a desencriptar: ")
-        
-    claveDescifrada=descifra(clave)
-    claveComprobada=cifra(claveDescifrada)
+    #La clave desencriptada es el valor de descifrar la clave
+    claveDesencriptada=descifra(clave)
+    #La clave Encriptada es el valor de cifrar la clave desencriptada
+    claveEncriptada=cifra(claveDesencriptada)
+
+    #Bucle que se repite siempre que la clave sea igual a la encriptada
+    # y que la clave no sea igual que la clave desencriptada
+    while clave==claveEncriptada and clave!=claveDesencriptada:
+        #En cada iteración, la clave toma el valor de la clave desencriptada
+        #de la iteración anterior
+        clave=claveDesencriptada
+        #Volvemos a desencriptar y encriptar la clave para la siguiente comprobación
+        claveDesencriptada=descifra(clave)
+        claveEncriptada=cifra(claveDesencriptada)
     
-    while clave==claveComprobada:
-        clave=claveDescifrada
-        claveDescifrada=descifra(claveDescifrada)
-        claveComprobada=cifra(claveDescifrada)
-        
-    print("El DNI es %s" % claveDescifrada)
+    #Imprimimos el resultado con el valor final de la clave desencriptada
+    print("El DNI es %s" % claveDesencriptada)
 
 main()
-        
+
